69024d76c465cc063811cb52624705efce7406a2
Jellatino <angelxo.25@gmail.com>
2025-02-28 17:00:46 +0800

    [Angel] Token and TokenType added

---
+++ src/main/java/com/example/bisayaplusplus/lexer/Token.java
@@ -0,0 +1,4 @@
+package com.example.bisayaplusplus.lexer;
+
+public class Token {
+}
---
+++ src/main/java/com/example/bisayaplusplus/lexer/TokenType.java
@@ -0,0 +1,16 @@
+package com.example.bisayaplusplus.lexer;
+
+public enum TokenType {
+// single char
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACKET, RIGHT_BRACKET,
+    ASTERISK, DIVIDE, MODULO, PLUS,MINUS,
+    GREATER_THAN, LESSER_THAN, EQUAL,
+
+// double char
+    GREATER_OR_EQUAL, LESSER_OR_EQUAL,
+
+
+// data types
+    NUMERO, LETRA, TINUOD, TIPIK
+
+}
